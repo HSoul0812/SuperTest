6b0f327875370daf60a35eda173f2d411d7ce208
HSoul0812 <hugesoul0813@gmail.com>
2023-05-26 22:59:37 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        white: '#fff',
+        black: '#413C5F',
+        purple: '#5845DD',
+        'dark-purple': '#A39FC1',
+        gray: '#8B85B1',
+        'dark-gray': '#817CA5',
+        'light-gray': '#C0BCDF',
+        'icon-dark-gray': '#87839F',
+        'icon-gray': '#C9C5E8',
+      },
+    },
+  },
+  plugins: [],
+}
