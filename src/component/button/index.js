eca0ddfe7b05d48d3312b324a5b3765d202b2fa9
HSoul0812 <hugesoul0813@gmail.com>
2023-05-27 15:54:43 -0700

    removed all console

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -1,17 +1,14 @@
-const Button = ({ onClick, disabled, children }) => {
-  console.log(disabled)
-  return (
-    <div
-      className={`cursor-pointer w-full text-2xl font-medium leading-[29px] text-center py-4 ${
-        disabled ? 'bg-dark-purple text-gray ' : 'bg-white text-black'
-      }`}
-      onClick={() => {
-        !disabled && onClick()
-      }}
-    >
-      {children}
-    </div>
-  )
-}
+const Button = ({ onClick, disabled, children }) => (
+  <div
+    className={`cursor-pointer w-full text-2xl font-medium leading-[29px] text-center py-4 ${
+      disabled ? 'bg-dark-purple text-gray ' : 'bg-white text-black'
+    }`}
+    onClick={() => {
+      !disabled && onClick()
+    }}
+  >
+    {children}
+  </div>
+)
 
 export default Button
